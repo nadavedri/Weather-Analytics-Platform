72251fcf60f4bb2dbc972e4ac2c95dea29a91572
nadavedri <nadav052433@gmail.com>
2025-04-14 17:57:01 +0300

    hole pipeline with cron

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,48 @@
 import { cities } from './data/cities';
 import fetchWeatherForCity from './etl/extract';
+import transformData from './etl/transform';
+import { loadWeatherData } from './etl/load';
+import cron from 'node-cron';
+import { executeSchema } from './db/runSchema';
 
-(async () => {
-  for (const city of cities) {
-    const weather = await fetchWeatherForCity(city);
-    console.log(weather);
-  }
-})();
+executeSchema()
+  .then(() => {
+    console.log('Schema created successfully.');
+  })
+  .catch((err) => {
+    console.error('Schema creation failed:', err);
+  });
+
+const etl = async () => {
+  console.log('Starting ETL process...');
+
+  await Promise.all(
+    cities.map(async (city) => {
+      try {
+        console.log(`Fetching weather data for ${city.name}...`);
+        const rawWeatherData = await fetchWeatherForCity(city);
+        if (!rawWeatherData) throw new Error('rawData is null');
+
+        const transformedData = transformData(rawWeatherData);
+        await loadWeatherData(transformedData);
+
+        console.log(`Weather data for ${city.name} processed successfully.`);
+      } catch (error) {
+        console.error(`Error processing data for ${city.name}:`, error);
+      }
+    })
+  );
+
+  console.log('ETL process completed.');
+};
+
+// Schedule the ETL process to run every hour
+cron.schedule('0 * * * *', () => {
+  console.log('Running ETL process every hour...');
+  etl()
+    .then(() => console.log('ETL process finished.'))
+    .catch((err) => console.error('Error during ETL process:', err));
+});
+
+// Start the process immediately
+etl().catch((err) => console.error('Error during initial ETL execution:', err));
