9cd99d310176a698e245f6a2a4f5f400949288b6
nadavedri <nadav052433@gmail.com>
2025-04-14 20:54:01 +0300

    prettier

--- src/config/index.ts
+++ src/config/index.ts
@@ -30,12 +30,11 @@ const config: AppConfig = {
     dbName: getEnv('POSTGRES_DB'),
     host: process.env.POSTGRES_HOST || 'localhost',
   },
-  alerts:{
-    listOfImportentCities: ["Tel Aviv", "New York", "London"],
+  alerts: {
+    listOfImportentCities: ['Tel Aviv', 'New York', 'London'],
     notificationTemperature: 35,
-    notificationWindSpeed: 50
-
-  }
+    notificationWindSpeed: 50,
+  },
 };
 
 export default config;
