189ed1ded69506b810076271d5f8f1a891a42454
nadavedri <nadav052433@gmail.com>
2025-04-14 14:33:55 +0300

    extract improve

--- src/etl/extract.ts
+++ src/etl/extract.ts
@@ -1,39 +1,47 @@
 import axios from 'axios';
 import axiosRetry from 'axios-retry';
-import { City, WeatherData } from '../types/weatherTypes';
-import config  from '../config/index';
+import { City, RawWeatherData } from '../types/weatherTypes';
+import config from '../config/index';
 
 axiosRetry(axios, {
-    retries: 3,
-    retryDelay: axiosRetry.exponentialDelay,
-    retryCondition: (error) =>
-      axiosRetry.isNetworkError(error) || (error.response?.status ?? 0)  >= 500,
-  });
+  retries: 3,
+  retryDelay: axiosRetry.exponentialDelay,
+  retryCondition: (error) =>
+    axiosRetry.isNetworkError(error) || (error.response?.status ?? 0) >= 500,
+});
 
-const fetchWeatherForCity = async (city: City): Promise<WeatherData | null> => {
-    const { latitude, longitude, name } = city;
+const fetchWeatherForCity = async (city: City): Promise<RawWeatherData | null> => {
+  const { latitude, longitude, name } = city;
 
-    const url = `${config.openMetro.baseUrl}?latitude=${latitude}&longitude=${longitude}&current_weather=${config.openMetro.currentWeather}&timezone=${config.openMetro.timezone}`;
-  
-    try {
-      const response = await axios.get(url);
-      const current = response.data.current_weather;
-  
-      const weather: WeatherData = {
-        cityName: name,
-        temperature: current.temperature,
-        windSpeed: current.windspeed,
-        windDirection: current.winddirection,
-        timestamp: current.time,
-      };
-  
-      console.log(`Weather fetched for ${name}`);
-      return weather;
-    } catch (err: any) {
-      console.error(`Failed to fetch weather for ${name}:`, err.message);
-      return null;
+  const url = `${config.openMetro.baseUrl}?latitude=${latitude}&longitude=${longitude}&current_weather=${config.openMetro.currentWeather}&timezone=${config.openMetro.timezone}`;
+
+  try {
+    const response = await axios.get(url);
+    const currentRawData = response.data.current_weather;
+    console.log(`Weather fetched for ${name}`);
+    const rawWeatherData: RawWeatherData = {
+      cityName: name,
+      ...currentRawData,
+      time: new Date(currentRawData.time),
+    };
+
+    return rawWeatherData;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      console.error(
+        `Failed to fetch weather for ${city.name}:`,
+        error.response?.status
+          ? `Status: ${error.response.status}, Message: ${error.message}`
+          : error.message
+      );
+    } else {
+      console.error(
+        `Unexpected error fetching weather for ${city.name}:`,
+        error instanceof Error ? error.message : String(error)
+      );
     }
-
+    return null;
+  }
 };
 
 export default fetchWeatherForCity;
