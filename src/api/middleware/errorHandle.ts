0d12dc5780c66e2bf11efc26e469ef6e5aece20f
nadavedri <nadav052433@gmail.com>
2025-04-14 23:44:43 +0300

    middleware errors to logger

--- src/api/middleware/errorHandle.ts
+++ src/api/middleware/errorHandle.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
+import logger from '../../logger';
 
 export const errorHandler = (err: any, _req: Request, res: Response, _next: NextFunction) => {
-  console.error('Error:', err.message || err);
+  logger.error('Error:', err.message || err);
   res.status(500).json({
     error: 'Internal Server Error',
     message: err.message || 'Something went wrong',
